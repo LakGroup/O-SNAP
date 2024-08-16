717453f7a2d9a94b63671f0abb04013cb8455335
hkim-upenn <hannah.kim3@pennmedicine.upenn.edu>
2024-08-16 11:09:11 -0400

    Deleted files; moved folders

--- NuCLiPSE/loadVars.m
+++
@@ -1,7 +0,0 @@
-% load specified variables from file
-function data = loadVars(filePath, varsToLoad)
-    idx = cellfun(@(x) ~isempty(who('-file', filePath,x)),varsToLoad,'uni',1);
-    varsToLoadInData = varsToLoad(idx);
-    data = load(filePath,varsToLoadInData{:});
-    % varsNotThere = varsToLoad(~idx);
-end
